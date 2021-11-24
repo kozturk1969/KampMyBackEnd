1c742716557d94b04bfb8ed8657a3004f2c94953
k_ozturk <k_ozturk@borcelik.com>
2021-11-24 16:49:59 +0300

    2021-11-24

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -20,17 +22,21 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _ProductDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _ProductDal = productDal;
+            _categoryService = categoryService;
         }
 
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //Business Codes
+
+            //////////////////////////////////////
+
             //validation
             //
 
@@ -44,8 +50,6 @@ namespace Business.Concrete
 
             //    return new ErrorResult(Messages.ProductNameInvalid );
             //}
-
-
 
 
 
@@ -64,41 +68,111 @@ namespace Business.Concrete
             //ValidationAspect ilede bu koda gerek kalmıyor
             //ValidationTool.Validate(new ProductValidator(), product);
 
+            //////////////////////////////////////
 
-            _ProductDal.Add(product);
+            //Business Rules Codes
+
+            ////Bir kategoride en fazla 15 ürün olabilir
+            //if (CheckIfProductOfCategoryCorrect(product.CategoryId).Success == false)
+            //{
+            //    return new ErrorResult();
+            //}
+
+            ////Aynı isimde ürün eklenemez
+            //if (CheckIfProductNameExist(product.ProductName).Success == false)
+            //{
+            //    return new ErrorResult();
+            //}
+
+           IResult result= BusinessRules.Run
+                (CheckIfProductNameExist(product.ProductName)
+                ,
+                CheckIfProductOfCategoryCorrect(product.CategoryId)
+                ,
+                CheckIfCategoryLimitExceded()
+                );
+
+            if (result !=null)
+            {
+                return result;
+
+            }
+
+                        _ProductDal.Add(product);
             return new Result(true, Messages.ProductAdded);
+
+
+
         }
 
         public IDataResult<List<Product>> GetAll()
         {
-            
-            if (DateTime.Now.Hour ==17)
+
+            if (DateTime.Now.Hour == 17)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
 
-            return new SuccessDataResult<List<Product>>( _ProductDal.GetAll() ,Messages.ProductListed );
+            return new SuccessDataResult<List<Product>>(_ProductDal.GetAll(), Messages.ProductListed);
 
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return  new SuccessDataResult<List<Product>>(_ProductDal.GetAll(p => p.CategoryId == id));
+            return new SuccessDataResult<List<Product>>(_ProductDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return  new SuccessDataResult<Product>(_ProductDal.Get(p => p.ProductId == productId));
+            return new SuccessDataResult<Product>(_ProductDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(_ProductDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice <=max));
+            return new SuccessDataResult<List<Product>>(_ProductDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_ProductDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private IResult CheckIfProductOfCategoryCorrect(int categoryId)
+        {
+            var result = _ProductDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 15)
+            {
+                return new ErrorResult(Messages.ProductionCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExist(string productName)
+        {
+            var result = _ProductDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+
+            if (result.Data.Count  > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,13 +9,16 @@ namespace Business.Constants
 {
 
     //Static olunca newlemeye gerek kalmıyor
-   public static  class Messages
+    public static class Messages
     {
-        public static string ProductListed ="Ürünler Listelendi";
-        public static string MaintenanceTime ="Bakım Yapılıyor";
+        public static string ProductListed = "Ürünler Listelendi";
+        public static string MaintenanceTime = "Bakım Yapılıyor";
 
         //Public değişkenler Pascal case yazılır
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ür,n ismi geçersiz";
+        public static string ProductionCountOfCategoryError = "Bir Kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExist = "Aynı isimde zaten başka  bir ürün var.";
+        public static string CategoryLimitExceded="Kategory limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
