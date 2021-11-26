d308039dedf53e8245cbcd377f09fe661deebb9a
k_ozturk <k_ozturk@borcelik.com>
2021-11-26 11:05:59 +0300

    2021-11-26 11:05

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,13 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+            serviceCollection.AddMemoryCache(); //MomoryCacheManager da IMemoryCache _memoryCache; satırının çalışabilmesi  için yazıldı.
+            serviceCollection.AddSingleton<ICacheManager, MomoryCacheManager>(); //bu da bizim cachemanagerimiz.
+                                                                                 //ileride Redid ile çalışmak istenirse RedisCacheManager modülü yaılmalı MomoryCacheManager ile değiştirilmeşli
+                                                                                 //Redis olursa serviceCollection.AddMemoryCache() satırına ihtiyaç kalmıyor
         }
     }
 }
